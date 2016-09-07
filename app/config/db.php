c5a02eb2b7aa4f1fea4265ec488e762d0144cfae
root <root@root-lapto>
2016-09-07 10:45:08 -0400

    Subiendo proyecto

---
+++ app/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=prueba',
+    'username' => 'esmir',
+    'password' => '2712',
+    'charset' => 'utf8',
+    'tablePrefix' => '',
+    'enableSchemaCache' => true,
+];
